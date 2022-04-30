20631b782296bae401a9b232239c980dce3821f4
Lukas Kusik <lukas.kusik@gmail.com>
2022-04-30 13:51:21 +0200

    toFlow extension function

--- lib/src/main/java/net/kusik/parseandroidx/flow/ParseFlow.kt
+++ lib/src/main/java/net/kusik/parseandroidx/flow/ParseFlow.kt
@@ -3,34 +3,28 @@ package net.kusik.parseandroidx.flow
 import com.parse.ParseObject
 import com.parse.ParseQuery
 import com.parse.livequery.ParseLiveQueryClient
-import com.parse.livequery.SubscriptionHandling
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
-import kotlinx.coroutines.flow.conflate
 
 /**
  *  Flow object upon a parse subscribeQuery, using Flows
  */
-class ParseFlow<T : ParseObject>(
-    private val subscribeQuery: ParseQuery<T>,
-    private val parseClient: ParseLiveQueryClient = ParseLiveQueryClient.Factory.getClient()
-) {
-    private lateinit var subscriptionHandling: SubscriptionHandling<T>
+fun <T : ParseObject?> ParseQuery<T>.toFlow(
+    parseClient: ParseLiveQueryClient = ParseLiveQueryClient.Factory.getClient()
+) = callbackFlow<List<T>> {
 
-    operator fun invoke() = callbackFlow<List<T>> {
-        // Subscribe to subscribeQuery
-        subscriptionHandling = parseClient.subscribe(subscribeQuery)
+    // Subscribe to subscribeQuery
+    val subscriptionHandling = parseClient.subscribe(this@toFlow)
 
-        subscriptionHandling.handleEvents { _, _, _ ->
-            // refresh On subscribeQuery change
-            subscribeQuery.findInBackground { objects, e ->
-                trySend(objects)
-            }
+    subscriptionHandling.handleEvents { _, _, _ ->
+        // refresh On subscribeQuery change
+        this@toFlow.findInBackground { objects, e ->
+            trySend(objects)
         }
+    }
 
-        awaitClose {
-            // Unsubscribe the subscription
-            parseClient.unsubscribe(subscribeQuery, subscriptionHandling)
-        }
-    }.conflate()
+    awaitClose {
+        // Unsubscribe the subscription
+        parseClient.unsubscribe(this@toFlow, subscriptionHandling)
+    }
 }
