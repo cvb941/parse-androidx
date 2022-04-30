3fb3ef3f3a199e45dede901e42ae9eb069f9a2e7
Lukas Kusik <lukas.kusik@gmail.com>
2022-04-30 15:18:52 +0200

    toFlow initial load

--- lib/src/main/java/net/kusik/parseandroidx/flow/ParseFlow.kt
+++ lib/src/main/java/net/kusik/parseandroidx/flow/ParseFlow.kt
@@ -6,13 +6,19 @@ import com.parse.livequery.ParseLiveQueryClient
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.yield
+import net.kusik.parseandroidx.coroutines.findSuspend
 
 /**
  *  Flow object upon a parse subscribeQuery, using Flows
  */
-fun <T : ParseObject?> ParseQuery<T>.toFlow(
+fun <T : ParseObject> ParseQuery<T>.toFlow(
     parseClient: ParseLiveQueryClient = ParseLiveQueryClient.Factory.getClient()
 ) = callbackFlow<List<T>> {
+
+    // LiveQuery only handles updates, load the initial object manually
+    send(this@toFlow.findSuspend())
+    yield() // Yield now, so that if the caller wanted only one value, we do not unnecessarily subscribe to updates
 
     // Subscribe to subscribeQuery
     val subscriptionHandling = parseClient.subscribe(this@toFlow)
@@ -33,7 +39,7 @@ fun <T : ParseObject?> ParseQuery<T>.toFlow(
 /**
  *  Flow object upon a parse subscribeQuery, using Flows
  */
-fun <T : ParseObject?> ParseQuery<T>.toFlowSingle(
+fun <T : ParseObject> ParseQuery<T>.toFlowSingle(
     parseClient: ParseLiveQueryClient = ParseLiveQueryClient.Factory.getClient()
 ) = toFlow(parseClient)
     .map { if (it.size > 1) throw IllegalStateException("Result of this parse query has multiple results") else it.firstOrNull() }
